eaacf24c99c8f790a4b9b17503fc2e8d284275ec
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-02-12 15:13:10 +0100

    Incorporacion de Tiled

--- CanColapiGDX/core/src/alex/mario/game/MyGdxGame.java
+++ CanColapiGDX/core/src/alex/mario/game/MyGdxGame.java
@@ -2,47 +2,122 @@ package alex.mario.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 
-public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch; //Buffer de pintado en tarjeta grafica
-	private Texture img;
-	private BitmapFont font;
-	private int width, height;
-	
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer tiledMapRenderer;
+
+    //Animacion player
+    Texture player;
+    MapLayer playerLayer;
+    TextureRegion textureRegion;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		font = new BitmapFont();
-		width = Gdx.graphics.getWidth();
-		height = Gdx.graphics.getHeight();
+	    //Obtencion de ancho y alto
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+        //Camara y tilemap
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		camera.update();
+		tiledMap = new TmxMapLoader().load("MapaTest.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
+
+        //Player
+        player = new Texture("pokeTest.png");
+        playerLayer = tiledMap.getLayers().get("Personajes");
+        textureRegion = new TextureRegion(player,28,38);
+
+        TextureMapObject tmo = new TextureMapObject(textureRegion);
+        tmo.setX(200);
+        tmo.setY(200);
+        playerLayer.getObjects().add(tmo);
 	}
 
 	@Override
 	public void render () {
-        renderizarPantalla();
-        procesarEntrada();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 	}
 
-    private void procesarEntrada() {
-    }
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
 
-    private void renderizarPantalla() {
-        Gdx.gl.glClearColor(0.5f, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        //batch.draw(img, 0, 0);
-        font.draw(batch, "hola", 100, 100);
-        batch.end();
-    }
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
 
     @Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
+        Vector3 position = camera.unproject(clickCoordinates);
+        TextureMapObject character = (TextureMapObject)tiledMap.getLayers().get("Personajes").getObjects().get(0);
+        character.setX((float)position.x);
+        character.setY((float)position.y);
+        return true;
+    }
+
+
+    @Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
