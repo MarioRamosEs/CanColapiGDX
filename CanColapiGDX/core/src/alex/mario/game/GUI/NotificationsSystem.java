3a2c6f29ac144afaaf2e421dcb508438527dee6e
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2018-02-16 17:54:33 +0100

    Clases creadas y vinculadas

---
+++ CanColapiGDX/core/src/alex/mario/game/GUI/NotificationsSystem.java
@@ -0,0 +1,6 @@
+package alex.mario.game.GUI;
+
+import alex.mario.game.LOGIC.NotificationsSystem_L;
+
+public class NotificationsSystem extends NotificationsSystem_L {
+}
---
+++ CanColapiGDX/core/src/alex/mario/game/LOGIC/NotificationsSystem_L.java
@@ -0,0 +1,4 @@
+package alex.mario.game.LOGIC;
+
+public class NotificationsSystem_L {
+}
