3e6672f683f2ce692e47627324d20ec69e5c3142
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2018-02-27 12:14:52 +0100

    BUGFIX y mejora inventario
    -La llave ahora muestra su código de llave
    -Bug arreglado doble llave recogida (el método por defecto del super ya
    lo hace)

--- CanColapiGDX/core/src/alex/mario/game/objects/Key.java
+++ CanColapiGDX/core/src/alex/mario/game/objects/Key.java
@@ -4,10 +4,15 @@ import alex.mario.game.GUI.Character;
 import alex.mario.game.GUI.Item;
 import alex.mario.game.GUI.TexturesSystem;
 import alex.mario.game.MyGdxGame;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Align;
 
 public class Key extends Item {
     protected String keyCode;
@@ -19,9 +24,30 @@ public class Key extends Item {
         this.keyCode = rectangleMapObject.getProperties().get("keyCode").toString();
     }
 
-    public void useGround(MyGdxGame game, Character character){
-        super.useGround(game, character);
-        character.addItem(this);
+    @Override
+    public void draw(ShapeRenderer shapeRenderer, SpriteBatch spriteBatch, BitmapFont font, Vector2 pos, Vector2 size)
+    {
+        shapeRenderer.begin();
+        //Calculate text width-height
+        this.notificationLayout.setText(font, this.name + " (" + this.keyCode + ")", Color.WHITE, size.x - 10, Align.left, true);
+
+        //Bakcground
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.GRAY);
+        shapeRenderer.rect(
+                pos.x, pos.y,
+                size.x, size.y
+        );
+
+        shapeRenderer.end();
+
+        spriteBatch.begin();
+        //Message
+        font.draw(spriteBatch, this.name + " (" + this.keyCode + ")",
+                pos.x + 5, pos.y + notificationLayout.height + 5,
+                size.x - 5, Align.center, true);
+
+        spriteBatch.end();
     }
     public String getKeyCode(){
         return this.keyCode;
