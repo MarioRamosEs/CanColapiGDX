99695d3e75784fd110c4c9ec4829b38a48b2e2fa
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2018-03-02 20:48:45 +0100

    Pruebas generando personajes - not done

--- CanColapiGDX/core/src/alex/mario/game/objects/CharacterSpawner.java
+++ CanColapiGDX/core/src/alex/mario/game/objects/CharacterSpawner.java
@@ -6,6 +6,7 @@ import alex.mario.game.GUI.Map;
 import alex.mario.game.GUI.TexturesSystem;
 import alex.mario.game.LOGIC.Character_L;
 import alex.mario.game.MyGdxGame;
+import alex.mario.game.characters.Dog;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -17,14 +18,18 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.ArrayList;
 
 public class CharacterSpawner extends Item {
+    private String classCharacter;
+    private boolean active = true;
     private int spawnEvery;
     private int quantity;
     private Long lastSpawn;
+    private int quantity_spawned = 0;
     public CharacterSpawner(MyGdxGame game, Map map, RectangleMapObject rectangleMapObject){
         super(game, map, rectangleMapObject);
         this.name = rectangleMapObject.getProperties().get("name", "CharacterSpawner", String.class);
         this.spawnEvery = rectangleMapObject.getProperties().get("spawnEvery", 5000, int.class);
         this.quantity = rectangleMapObject.getProperties().get("quantity", 2, int.class);
+        this.classCharacter = rectangleMapObject.getProperties().get("classCharacter", "UNDEFINED", String.class);
 
         this.size = MyGdxGame.DEFAULT_TILE_SIZE;
 
@@ -50,9 +55,19 @@ public class CharacterSpawner extends Item {
     }
     @Override
     public void update(){
+        if(!active){return;}
+
         if( (lastSpawn + this.spawnEvery) < System.currentTimeMillis()){
+            System.out.println("SPAWNED");
             this.lastSpawn = System.currentTimeMillis();
-            this.game.addCharacter(new Character(this.game, this.map));
+            Character character = new Dog(this.game, this.map);
+            character.setPos(this.pos);
+            System.out.println(this.name);
+            this.game.addCharacter(character);
+            this.quantity_spawned++;
+            if(this.quantity_spawned > this.quantity){
+                this.active = false;
+            }
         }
     }
 }
