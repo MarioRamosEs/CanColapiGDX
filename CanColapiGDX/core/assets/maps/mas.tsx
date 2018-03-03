c8714e06419f81eda21d625b26fc191e29e59c6a
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-03-03 20:16:34 +0100

    Tiles sobrantes eliminados y update planta2

--- CanColapiGDX/core/assets/maps/mas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mas" tilewidth="32" tileheight="32" tilecount="504" columns="24">
- <image source="mas.png" width="768" height="672"/>
-</tileset>
