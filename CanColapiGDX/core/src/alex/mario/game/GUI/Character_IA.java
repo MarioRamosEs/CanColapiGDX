d39c8627637045858a5bc72eaab4015bcf194ecd
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2018-03-03 17:07:53 +0100

    El personaje IA lee la capa path y toma la direccion que dice

--- CanColapiGDX/core/src/alex/mario/game/GUI/Character_IA.java
+++ CanColapiGDX/core/src/alex/mario/game/GUI/Character_IA.java
@@ -2,6 +2,14 @@ package alex.mario.game.GUI;
 
 import alex.mario.game.Interfaces.iCharacter_IA_L;
 import alex.mario.game.MyGdxGame;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.Iterator;
 
 public class Character_IA extends Character implements iCharacter_IA_L {
     public Character_IA(MyGdxGame game, Map map) {
@@ -11,5 +19,15 @@ public class Character_IA extends Character implements iCharacter_IA_L {
     @Override
     public void think() {
 
+        MapLayer PathObjectLayer = this.map.getTiledMap().getLayers().get("Path");
+        MapObjects paths = PathObjectLayer.getObjects();
+
+        for (RectangleMapObject rectangleObject : paths.getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = rectangleObject.getRectangle();
+            MapProperties mapProperties = rectangleObject.getProperties();
+            if(Intersector.overlaps(rectangle, this.getRect())){
+                this.direction = MyGdxGame.getDirectionFromString(mapProperties.get("direction", "undefined", String.class));
+            }
+        }
     }
 }
